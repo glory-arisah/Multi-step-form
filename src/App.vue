93a9ca0bc19d4d3bf8367eb41f0e7f4b62e767bf
Glory Arisah <garisah@ipnxnigeria.net>
2023-05-24 15:55:29 +0100

    project set up deleted stock files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="main">
+    <header class="header">
+      <h1>Starting a new project? Let's work on it!</h1>
+      <h4>
+        Please fill this short form to get started, we will reach out to you
+        shortly
+      </h4>
+    </header>
+    <section class="form-container">content</section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.main {
+  min-height: 100vh;
+  background: linear-gradient(to bottom right, #7f3adf, #90a0f9);
+  color: #ffd;
+}
+
+.header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 3rem;
+}
+.header h4 {
+  margin-top: 0.8rem;
+}
+
+.form-container {
+  background-color: #fff;
+  border-radius: 0.8rem;
+  margin: 2rem auto 0 auto;
+  max-width: 65%;
+  padding: 1rem;
+  color: #000;
 }
 </style>
